dee97c83379e7188b31caab67240a13e3c8ddc8a
CloudlyC <Cloudlyc829@gmail.com>
2026-02-02 17:12:43 -0800

    testimonials functionality

--- js/main.js
+++ js/main.js
@@ -6,6 +6,15 @@ const card = document.querySelector('.card');
 const style = window.getComputedStyle(card);
 const cardWidth = card.offsetWidth + parseInt(style.marginRight);
 
+
+const reviewsTrack = document.querySelector('.reviews-scroll');
+const reviewsNext = document.querySelector('.review-next');
+const reviewsPrev = document.querySelector('.review-prev');
+
+const reviewCard = document.querySelector('.review-card');
+const reviewStyle = window.getComputedStyle(reviewCard);
+const reviewCardWidth = reviewCard.offsetWidth + parseInt(reviewStyle.marginRight);
+
 // Next button scrolls right
 nextBtn.addEventListener('click', () => {
   track.scrollBy({ left: cardWidth, behavior: 'smooth' });
@@ -14,5 +23,16 @@ nextBtn.addEventListener('click', () => {
 // Prev button scrolls left
 prevBtn.addEventListener('click', () => {
   track.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+});
+
+
+// Scroll right
+reviewsNext.addEventListener('click', () => {
+    reviewsTrack.scrollBy({left: reviewCardWidth, behavior: 'smooth'});
+});
+
+// Scroll left
+reviewsPrev.addEventListener('click', () => {
+    reviewsTrack.scrollBy({left: -reviewCardWidth, behavior: 'smooth'});
 });
 
